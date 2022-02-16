f0292eec29988cad5dc8770b871e6afabe9ce7a9
Julia Norwill <julianorwill@gmail.com>
2022-02-16 19:19:04 +0300

    feat: added a script for the page with categories

--- src/components/gamepage/gamePage.ts
+++ src/components/gamepage/gamePage.ts
@@ -1,16 +1,49 @@
-import './style_gamepage.scss'
-export class GamePage {
-  head: HTMLParagraphElement;
-  wrapper: HTMLDivElement;
+import categoryHtml from "./category.html";
+import "./index.scss";
+interface Iword {
+  audio: string;
+  audioExample: string;
+  audioMeaning: string;
+  group: number;
+  id: string;
+  image: string;
+  page: number;
+  textExample: string;
+  textExampleTranslate: string;
+  textMeaning: string;
+  textMeaningTranslate: string;
+  transcription: string;
+  word: string;
+  wordTranslate: string;
+}
+export class SprintPage {
   node: HTMLElement;
+  category: HTMLDivElement;
   constructor(node: HTMLElement) {
     this.node = node;
-    this.wrapper = document.createElement('div');
-    this.head = document.createElement('p');
-    this.wrapper.appendChild(this.head);
-  };
-  render(nameOfPage: string) {
-    this.head.textContent = (`${nameOfPage}`);
-    this.node.appendChild(this.wrapper);
+    this.category = document.createElement("div");
+    this.category.classList.add("category");
+    this.category.innerHTML = categoryHtml;
+  }
+
+  chooseLvl() {
+    const arrLvlButton = document.querySelectorAll(".category__item");
+    arrLvlButton.forEach((button: HTMLButtonElement) => {
+      button.onclick = () => {
+        localStorage.setItem("lvl", `${button.dataset.lvl}`);
+      };
+    });
+  }
+
+  renderCategory() {
+    this.node.appendChild(this.category);
+    this.chooseLvl();
+  }
+
+  destroyCard() {
+    this.node.innerHTML = "";
+  }
+  destroy() {
+    this.node.innerHTML = "";
   }
 }
