aa6ba33f8382876dba645f54da7dbe4c39427d1d
Julia Norwill <julianorwill@gmail.com>
2022-02-22 13:22:37 +0300

    fix: clarified statistics

--- src/components/statisticpage/statisticPage.ts
+++ src/components/statisticpage/statisticPage.ts
@@ -4,6 +4,7 @@ import "./index.scss";
 export class StatisticPage {
   wrapper: HTMLDivElement;
   node: HTMLElement;
+  name: HTMLSpanElement;
   learned: HTMLDivElement;
   right: HTMLDivElement;
   aunew: HTMLSpanElement;
@@ -17,8 +18,9 @@ export class StatisticPage {
     const wrapper = document.createElement("div");
     wrapper.classList.add("statistics");
     wrapper.innerHTML = statisticsHTML;
+    this.name = wrapper.querySelector(".statistics__name");
     this.learned = wrapper.querySelector(".statistics__learned");
-    this.right = wrapper.querySelector(".statistics__learned");
+    this.right = wrapper.querySelector(".statistics__right");
 
     this.aunew = wrapper.querySelector(".statistics-game__audiocall_new");
     this.auright = wrapper.querySelector(".statistics-game__audiocall_right");
@@ -31,19 +33,29 @@ export class StatisticPage {
   }
   async render() {
     this.destroy();
+    this.name.innerHTML = JSON.parse(localStorage.getItem("user")).name;
     const myApi = new OldStatistics();
     await myApi.getStatistics();
+    console.log('myApi', myApi)
+    if (myApi.learnedWordsNumber) {
+      this.learned.innerHTML = `${myApi.learnedWordsNumber}`;
+    } else this.learned.innerHTML = "0";
+    if (myApi.allRaunds) {
+      this.right.innerHTML = `${Math.floor(
+        (myApi.allRightAnswers * 100) / myApi.allRaunds
+      )}`;
+    } else this.right.innerHTML = "0";
+    if (myApi.allRaundsAudiocall) {
+      this.auright.innerHTML = `${Math.floor(
+        (myApi.RightAnswersAudiocall * 100) / myApi.allRaundsAudiocall
+      )}`;
+    } else this.auright.innerHTML = "0";
 
-    this.learned.innerHTML = `${myApi.learnedWordsNumber}`;
-    this.right.innerHTML = `${Math.floor(
-      (myApi.allRightAnswers * 100) / myApi.allRaunds
-    )}`;
-    this.auright.innerHTML = `${Math.floor(
-      (myApi.RightAnswersAudiocall * 100) / myApi.allRaundsAudiocall
-    )}`;
-    this.spright.innerHTML = `${Math.floor(
-      (myApi.RightAnswersSprint * 100) / myApi.allRaundsSprint
-    )}`;
+    if (myApi.allRaundsSprint) {
+      this.spright.innerHTML = `${Math.floor(
+        (myApi.RightAnswersSprint * 100) / myApi.allRaundsSprint
+      )}`;
+    } else this.spright.innerHTML = "0";
     this.spinRow.innerHTML = `${myApi.correctAnswerInRowInSprint}`;
     this.spnew.innerHTML = `${myApi.newWordsSprint.length}`;
     this.auinRow.innerHTML = `${myApi.correctAnswerInRowInAudiocall}`;
